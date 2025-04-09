6a8ae077fc1fbcca04a61fea6151893984223235
palakrawat30 <nokatakamepalak@gmail.com>
2025-04-09 20:53:08 +0530

    Day09.py
    Here we are taking a secret message from user then reversing it using slicing and printing the message.

---
+++ submissions/Palak_Rawat/Day09.py
@@ -0,0 +1,3 @@
+n=input("Enter the secret message: ")     #Taking user secret message using input() function
+reverse=n[::-1]                  #Reversing the n string given by user using slicing of string 
+print(reverse)
